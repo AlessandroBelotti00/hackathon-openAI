af976ebf439aaf66eeb03244ad0d09bce45f998f
a.belotti29 <abelotti00@gmail.com>
2023-03-01 22:17:31 +0100

    Update App.py
    Suddivisione dei topic nel syllabus e richiesta delle relative lezioni a gpt

--- App/app.py
+++ App/app.py
@@ -2,12 +2,14 @@ import os
 import streamlit as st
 from openai_utils.openaiAPI import OpenAIAPI
 from dotenv import load_dotenv
+from api import call_api
 
 load_dotenv()
-OPENAI_KEY_VAR=os.getenv('openai_key')
+OPENAI_KEY_VAR="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"
+#os.getenv('openai_key')
 
 ## To be done: read from json/csv
-subjects_list = ['English', 'Math', 'Science', 'Art']
+subjects_list = ['English', 'Math', 'Science', 'Art', 'Lesson_Example']
 st.title("Teacher AI")
 st.markdown(
     "This mini-app generates elementary school lessons using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview)."
@@ -17,27 +19,24 @@ col1, col2 = st.columns(2)
 
 with col1:
 
-    fileObject = open("Example/lesson_example.txt", "r")
-    data = fileObject.read()
-
     option = st.selectbox(
         'Select the subject',
         subjects_list)
 
+    path = "../Example/" + option.lower() + "syllabus.txt"
+    print(path)
+    fileObject = open(path, "r")
+    data = fileObject.read()
 
-    if option == 'Math':
-        st.write('You selected:', data)
-
-
-    text_input = st.text_input(
-        "GPT3 text 👇",
-    #   label_visibility=st.session_state.visibility,
-    #   disabled=st.session_state.disabled,
-    #   placeholder=st.session_state.placeholder,
-    )
-    if text_input:
+    pos = data.find("Week" ,0)
+    for i in range(6):
+        new_pos = data.find("Week" ,pos+3)
+        st.write(data[pos:new_pos])
         oai = OpenAIAPI(OPENAI_KEY_VAR)
-        st.write("Answer\n", oai.call_API(text_input))
+        st.write("EXPLANATION:\n", oai.call_API("explain me as a teacher in a complete form the concept of "+data[pos+7:new_pos]))
+        pos = new_pos
+
+        
 
 
 with col2:
