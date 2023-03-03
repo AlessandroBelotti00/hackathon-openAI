3defec6ba8ddfb71a8101abfb94cd7ed51fa0e9b
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-03-03 16:20:40 +0200

    fixed paths

--- App/app.py
+++ App/app.py
@@ -3,12 +3,13 @@ import streamlit as st
 from openai_utils.openaiAPI import OpenAIAPI
 from dotenv import load_dotenv
 from read import *
+import platform
 
 load_dotenv()
 OPENAI_KEY_VAR=os.getenv('openai_key')
 
 ## To be done: read from json/csv
-subjects_list = ['En    rt']
+subjects_list = ['English', 'Math', 'Science']
 st.title("Teacher AI")
 st.markdown(
     "This mini-app generates elementary school lessons using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview)."
@@ -25,16 +26,22 @@ with col1:
         'Select the subject',
         subjects_list)
 
-    if st.button('Play'):
-        st.write("bottono premuto, pre stopSpeach")
-        stopSpeach()
+    #if st.button('Play'):
+     #   st.write("bottono premuto, pre stopSpeach")
+      #  stopSpeach()
+    if platform.system()=="Windows":
+        path = "../Example/" + option.lower() + "syllabus.txt"
+    else:
+        path = os.getcwd()+"/Example/"+ option.lower()+"syllabus.txt"
 
-    path = "../Example/" + option.lower() + "syllabus.txt"
+    # Print the shortened path
+    print("Short Path:", path)
+
     print(path)
     fileObject = open(path, "r")
     data = fileObject.read()
 
-    pos = data.find("Week" ,0)
+    pos = data.find("Week", 0)
     for i in range(6):
         new_pos = data.find("Week" ,pos+3)
         st.write(data[pos:new_pos])
